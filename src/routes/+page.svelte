b3ab1115d4b64a34938e1c0b505bea59e42c1581
CrispyBacon1999 <bacon.josh09@gmail.com>
2022-12-19 16:53:08 -0500

    Use actual physics calculations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,15 @@
 <script>
 	import * as Pancake from '@sveltejs/pancake';
-	import { onMount } from 'svelte';
 
 	let currentMode = 'bangbang';
+	let currentMotor = 'falcon500';
+	let gearRatio = 1;
+	let motorCount = 1;
+	let mass = 2; // kg
+	let radius = 2; // in
+
+	$: radiusMeters = radius / 39.37;
+	$: currentMotorData = motors[currentMotor];
 
 	let startPoint = 0;
 	let setpoint = 1500;
@@ -11,6 +18,12 @@
 	let outputPercentOff = -1;
 	let friction = 0.01;
 	let minSpeed = 30;
+
+	const motors = {
+		falcon500: {
+			torque: (rpm) => 4.688 - rpm * (4.688 / 6379.663)
+		}
+	};
 
 	let deltaTime = 0.02;
 	let simulationTime = 5;
@@ -26,13 +39,28 @@
 	let kI = 0.00016;
 	let kD = 0.00005;
 
-	$: simulate(currentMode, startPoint, setpoint, maxAcceleration, kP, kI, kD, simulationTime);
+	$: simulate(
+		currentMode,
+		startPoint,
+		setpoint,
+		currentMotorData,
+		motorCount,
+		gearRatio,
+		mass,
+		kP,
+		kI,
+		kD,
+		simulationTime
+	);
 
 	function simulate(
 		currentMode,
 		startPoint,
 		setpoint,
-		maxAcceleration,
+		currentMotorData,
+		motorCount,
+		gearRatio,
+		mass,
 		kP,
 		kI,
 		kD,
@@ -43,6 +71,7 @@
 		kD = kD === '' ? 0 : kD;
 		let data = [];
 		let currentVelocity = startPoint;
+		let previousVelocity = startPoint;
 		let previousError = 0;
 		let integral = 0;
 		for (let time = 0; time < simulationTime; time += deltaTime) {
@@ -63,26 +92,31 @@
 				integral = int;
 				previousError = error;
 			}
-			currentVelocity += output * maxAcceleration * deltaTime;
+
+			// Momentum
+			let momentOfInertia = (mass * Math.pow(radiusMeters, 2)) / 2; // kg * m^2
+			let torque = currentMotorData.torque(currentVelocity) * motorCount * gearRatio * output; // Nm
+			let acceleration = torque / momentOfInertia; // rad/s/s
+
+			currentVelocity += acceleration * deltaTime;
+
 			// Apply friction
 			let frictionOut = friction * currentVelocity;
 			currentVelocity -= frictionOut;
 
-			// Minimum speed due to friction
-			if (Math.abs(currentVelocity) < minSpeed) {
-				currentVelocity = 0;
-			}
+			previousVelocity = currentVelocity;
 
 			// Log data
 			data.push({
 				time,
 				currentVelocity,
+				torque,
 				output,
 				integral,
 				previousError
 			});
 		}
-		console.log(data);
+		// console.log(data);
 		graphData = data;
 	}
 
@@ -107,44 +141,74 @@
 	};
 </script>
 
-{#if currentMode === 'bangbang'}
-	<h1>Bang Bang (No PID Control)</h1>
-{:else if currentMode === 'pid'}
-	<h1>PID Control</h1>
-{/if}
+<div class="controls">
+	<section class="setup">
+		<div class="col">
+			{#if currentMode === 'bangbang'}
+				<h1>Bang Bang (No PID Control)</h1>
+			{:else if currentMode === 'pid'}
+				<h1>PID Control</h1>
+			{/if}
 
-<select bind:value={currentMode}>
-	<option value="bangbang">Bang Bang</option>
-	<option value="pid">PID</option>
-</select>
+			<select bind:value={currentMode}>
+				<option value="bangbang">Bang Bang</option>
+				<option value="pid">PID</option>
+			</select>
 
-<div>
-	<label class="form-label" for="setpoint">Start Speed</label>
-	<input bind:value={startPoint} type="number" name="setpoint" step={250} />
+			<div>
+				<label class="form-label" for="setpoint">Start Speed</label>
+				<input bind:value={startPoint} type="number" name="setpoint" step={250} />
+			</div>
+			<div>
+				<label class="form-label" for="setpoint">Setpoint</label>
+				<input bind:value={setpoint} type="number" name="setpoint" step={250} />
+			</div>
+			<div>
+				<label class="form-label" for="mass">Flywheel Mass</label>
+				<input bind:value={mass} type="number" name="mass" step={1} />
+			</div>
+			<div>
+				<label class="form-label" for="gearratio">Gear Ratio</label>
+				<input bind:value={gearRatio} type="number" name="gearratio" step={0.5} />
+			</div>
+			<div>
+				<label class="form-label" for="motorCount">Motor Count</label>
+				<input bind:value={motorCount} type="number" name="motorCount" step={1} />
+			</div>
+			<div>
+				<label class="form-label" for="simtime">Simulation Time</label>
+				<input bind:value={simulationTime} type="number" name="simtime" step={1} min={1} max={50} />
+			</div>
+		</div>
+		<div class="col">
+			{#if currentMode === 'pid'}
+				<div>
+					<label for="kP" class="form-label">P</label>
+					<input bind:value={kP} name="kP" type="number" step={0.01} />
+				</div>
+				<div>
+					<label for="kI" class="form-label">I</label>
+					<input bind:value={kI} name="kI" type="number" step={0.00001} />
+				</div>
+				<div>
+					<label for="kD" class="form-label">D</label>
+					<input bind:value={kD} name="kD" type="number" step={0.00001} />
+				</div>
+			{/if}
+		</div>
+	</section>
+	<section>
+		<div class="legend">
+			<h2>Chart Legend</h2>
+			<p class="legend-item setpoint">Setpoint</p>
+			<p class="legend-item velocity">Velocity</p>
+			<p class="legend-item output">Output Percent</p>
+			<p class="legend-item torque">Torque</p>
+			<p class="legend-item integral">Integral</p>
+			<p class="legend-item error">Error</p>
+		</div>
+	</section>
 </div>
-<div>
-	<label class="form-label" for="setpoint">Setpoint</label>
-	<input bind:value={setpoint} type="number" name="setpoint" step={250} />
-</div>
-<div>
-	<label class="form-label" for="simtime">Simulation Time</label>
-	<input bind:value={simulationTime} type="number" name="simtime" step={1} min={1} max={50} />
-</div>
-
-{#if currentMode === 'pid'}
-	<div>
-		<label for="kP" class="form-label">P</label>
-		<input bind:value={kP} name="kP" type="number" step={0.01} />
-	</div>
-	<div>
-		<label for="kI" class="form-label">I</label>
-		<input bind:value={kI} name="kI" type="number" step={0.00001} />
-	</div>
-	<div>
-		<label for="kD" class="form-label">D</label>
-		<input bind:value={kD} name="kD" type="number" step={0.00001} />
-	</div>
-{/if}
 
 <div class="chart">
 	<Pancake.Chart x1={minX} x2={maxX} y1={minY} y2={maxY}>
@@ -166,6 +230,14 @@
 			</Pancake.SvgLine>
 			<Pancake.SvgLine data={graphData} x={(d) => d.time} y={(d) => d.currentVelocity} let:d>
 				<path class="velocity" {d} />
+			</Pancake.SvgLine>
+			<Pancake.SvgLine
+				data={graphData}
+				x={(d) => d.time}
+				y={(d) => (d.torque * setpoint) / currentMotorData.torque(0)}
+				let:d
+			>
+				<path class="torque" {d} />
 			</Pancake.SvgLine>
 			{#if currentMode === 'pid'}
 				<Pancake.SvgLine data={graphData} x={(d) => d.time} y={(d) => d.integral} let:d>
@@ -196,6 +268,7 @@
 		padding: 3em 0 2em 2em;
 		margin: 0 0 36px 0;
 		max-width: 80em;
+		overflow: hidden;
 	}
 	.grid-line {
 		position: relative;
@@ -229,6 +302,14 @@
 		fill: none;
 		stroke-dasharray: 8;
 	}
+	path.torque {
+		stroke: cyan;
+		opacity: 1;
+		stroke-linejoin: round;
+		stroke-linecap: round;
+		stroke-width: 2px;
+		fill: none;
+	}
 	path.velocity {
 		stroke: red;
 		opacity: 1;
@@ -239,15 +320,15 @@
 	}
 	path.output {
 		stroke: blue;
-		opacity: 1;
+		opacity: 0.5;
 		stroke-linejoin: round;
 		stroke-linecap: round;
-		stroke-width: 2px;
+		stroke-width: 1px;
 		fill: none;
 	}
 
 	path.previousError {
-		stroke: #999;
+		stroke: gray;
 		opacity: 1;
 		stroke-linejoin: round;
 		stroke-linecap: round;
@@ -295,6 +376,51 @@
 	.tooltip strong {
 		font-size: 1.4em;
 		display: block;
+	}
+
+	.legend {
+		border: 1px solid black;
+		padding: 1em;
+	}
+
+	.legend-item:before {
+		display: inline-block;
+		width: 0.75em;
+		height: 0.75em;
+		margin-right: 0.5em;
+		content: '';
+	}
+
+	.legend-item.velocity:before {
+		background-color: red;
+	}
+	.legend-item.output:before {
+		background-color: blue;
+		opacity: 0.5;
+	}
+	.legend-item.torque:before {
+		background-color: cyan;
+	}
+	.legend-item.integral:before {
+		background-color: green;
+		opacity: 0.5;
+	}
+	.legend-item.error:before {
+		background-color: gray;
+	}
+	.legend-item.setpoint:before {
+		background-color: black;
+	}
+	.controls {
+		display: flex;
+		flex-direction: row;
+		gap: 2em;
+	}
+
+	.setup {
+		display: flex;
+		flex-direction: row;
+		gap: 1em;
 	}
 
 	@media (min-width: 800px) {
