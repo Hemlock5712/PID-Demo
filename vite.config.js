77f077d90111ac8548e846d21c011c19ff4401cf
CrispyBacon1999 <bacon.josh09@gmail.com>
2022-12-19 15:34:59 -0500

    Vercel adapter

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	kit: {
+		adapter: adapter()
+	}
 };
 
 export default config;
